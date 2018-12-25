2cc5a65fa38d7031a2b0c9939744a89050787279
Puyan Wei <puyanwei@gmail.com>
2018-12-25 01:12:47 +0000

    Setup canvas to be centered even upon window resizing

--- src/sketch.js
+++ src/sketch.js
@@ -1,6 +1,18 @@
+let cnv;
 function setup() {
-    createCanvas(800, 800);
+    cnv = createCanvas(400, 800);
     background(100);
+    centerCanvas();
+}
+
+function centerCanvas() {
+    let x = (windowWidth - width) / 2;
+    let y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+}
+
+function windowResized() {
+    centerCanvas();
 }
 
 function draw() {}

5233c3b2becab4de9e8ee9b236f8e7129483defc
Puyan Wei <puyanwei@gmail.com>
2019-01-06 20:38:55 +0000

    Trying to create immutable 2d array unsuccessful

--- src/board.js
+++ src/board.js
@@ -1,35 +1,48 @@
 class Board {
-    constructor(columns, rows) {
-        this.columns = columns;
-        this.rows = rows;
-        this.grid;
-    }
+	constructor(columns, rows) {
+		this.columns = columns;
+		this.rows = rows;
+		this.grid;
+	}
 
-    initialize() {
-        let cell = new Cell();
-        let colsArray = new Array(this.columns).fill(cell);
-        this.grid = new Array(this.rows).fill(colsArray);
-    }
+	initialize() {
+		let cell = new Cell();
 
-    update(cellX, cellY) {
-        let indexX = cellX / gridSize;
-        let indexY = cellY / gridSize;
-        console.log(indexX, indexY, this.grid, this.grid[indexY][indexX]);
-        this.grid[indexY][indexX].isTaken = true;
-        console.log(this.grid);
-    }
+		let array = [];
+		let cols = [];
 
-    show() {
-        this.grid.map((row, rowIndex) => {
-            row.map((singleCell, colIndex) => {
-                singleCell.isTaken ? fill(255) : noFill();
-                rect(
-                    colIndex * gridSize,
-                    rowIndex * gridSize,
-                    gridSize,
-                    gridSize
-                );
-            });
-        });
-    }
+		for (let i = 0; i < this.columns; i++) {
+			cols = this.immutablePush(cols, cell);
+		}
+
+		for (let i = 0; i < this.rows; i++) {
+			array = this.immutablePush(array, cols);
+		}
+
+		this.grid = array;
+		console.log(array, cols);
+
+		// let columnArray = Array.from({ length: this.columns }, (element) => ...cell);
+		// this.grid = Array.from({ length: this.rows }, (element) => ...columnArray);
+	}
+
+	immutablePush(array, element) {
+		return [ ...array, element ];
+	}
+
+	update(cellX, cellY) {
+		let indexX = cellX / gridSize;
+		let indexY = cellY / gridSize;
+		this.grid[indexY][indexX].taken = true;
+		console.log(indexX, indexY, this.grid);
+	}
+
+	show() {
+		this.grid.map((row, rowIndex) => {
+			row.map((singleCell, colIndex) => {
+				singleCell.taken ? fill(255) : noFill();
+				rect(colIndex * gridSize, rowIndex * gridSize, gridSize, gridSize);
+			});
+		});
+	}
 }
--- src/sketch.js
+++ src/sketch.js
@@ -2,66 +2,61 @@ let canvas, tetromino, gridSize, board;
 let fallingShapes = new Array();
 
 function setup() {
-    canvas = createCanvas(400, 800);
-    centerCanvas();
-    frameRate(5); // Initial speed of level
+	canvas = createCanvas(400, 800);
+	centerCanvas();
+	frameRate(5); // Initial speed of level
 
-    gridSize = 40;
-    board = new Board(width / gridSize, height / gridSize);
-    board.initialize();
-    createShape();
+	gridSize = 40;
+	board = new Board(width / gridSize, height / gridSize);
+	board.initialize();
+	createShape();
 }
 
 function draw() {
-    let fallingShape = fallingShapes[0];
-    background(100);
-    fallingShape.update();
-    fallingShape.show();
-    checkIfLanded(fallingShape);
-    board.show();
+	let fallingShape = fallingShapes[0];
+	background(100);
+	fallingShape.update();
+	fallingShape.show();
+	checkIfLanded(fallingShape);
+	// board.show();
 }
 
 function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-        fallingTetominoes[0].setDir(-gridSize, 0);
-    }
-    if (keyCode === RIGHT_ARROW) {
-        fallingTetominoes[0].setDir(gridSize, 0);
-    }
-    if (keyCode === DOWN_ARROW) {
-        fallingTetominoes[0].setDir(0, gridSize * 2);
-    }
-    if (keyCode === UP_ARROW) {
-        // TODO goes straight to bottom
-    }
+	if (keyCode === LEFT_ARROW) {
+		fallingTetominoes[0].setDir(-gridSize, 0);
+	}
+	if (keyCode === RIGHT_ARROW) {
+		fallingTetominoes[0].setDir(gridSize, 0);
+	}
+	if (keyCode === DOWN_ARROW) {
+		fallingTetominoes[0].setDir(0, gridSize * 2);
+	}
+	if (keyCode === UP_ARROW) {
+		// TODO goes straight to bottom
+	}
 }
 
 function createShape() {
-    let piece = new Tetromino(
-        width / 2 - gridSize,
-        -gridSize,
-        gridSize,
-        gridSize
-    );
-    fallingShapes.push(piece);
+	let piece = new Tetromino(width / 2 - gridSize, -gridSize, gridSize, gridSize);
+	fallingShapes.push(piece);
 }
 
 function checkIfLanded(fallingShape) {
-    if (fallingShape.hasLanded()) {
-        board.update(fallingShape.x, fallingShape.y);
-        // TODO gotta move this tetromino object to the board object
-        fallingShapes.pop(1);
-        noLoop();
-        // createShape();
-    }
+	if (fallingShape.hasLanded()) {
+		board.update(fallingShape.x, fallingShape.y);
+		// TODO gotta move this tetromino object to the board object
+		fallingShapes.pop(1);
+		noLoop();
+		// createShape();
+	}
 }
 
 function centerCanvas() {
-    let x = (windowWidth - width) / 2;
-    let y = (windowHeight - height) / 2;
-    canvas.position(x, y);
+	let x = (windowWidth - width) / 2;
+	let y = (windowHeight - height) / 2;
+	canvas.position(x, y);
 }
 
 function windowResized() {
-    centerCanvas();
+	centerCanvas();
 }

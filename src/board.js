e071f1df3bd78a6039abcf91b1df8aab37cc9306
Puyan Wei <puyan.wei@mountainwarehouse.com>
2019-01-07 14:45:28 +0000

    Fixed 2d array to not be copied references but invidividual of each other

--- src/board.js
+++ src/board.js
@@ -1,57 +1,37 @@
 class Board {
-	constructor(columns, rows) {
-		this.columns = columns;
-		this.rows = rows;
-		this.grid;
-	}
+    constructor(columns, rows) {
+        this.columns = columns;
+        this.rows = rows;
+        this.grid;
+    }
 
-	initialize() {
-		let cell = new Cell();
+    initialize() {
+        this.grid = Array.from({ length: this.rows }, () =>
+            Array.from({ length: this.columns }, () => {
+                return new Cell();
+            })
+        );
+        console.log(this.grid);
+    }
 
-		// let array = [];
-		// let cols = [];
+    update(cellX, cellY) {
+        let indexX = cellX / gridSize;
+        let indexY = cellY / gridSize;
+        this.grid[indexY][indexX].taken = true;
+        console.log(indexX, indexY, this.grid);
+    }
 
-		// for (let i = 0; i < this.columns; i++) {
-		// 	cols = this.immutablePush(cols, cell);
-		// }
-
-		// for (let i = 0; i < this.rows; i++) {
-		// 	array = this.immutablePush(array, cols);
-		// }
-
-		// this.grid = array;
-		// console.log(array, cols);
-		let arrayRow = Array.from({ length: this.columns }, () => {
-			return cell;
-		});
-
-		let arrayOfRows = Array.from({ length: this.rows }, () => arrayRow);
-		this.grid = arrayOfRows;
-	}
-
-	//     var fruits = Array.from(
-	//         { length: 4 },
-	//         () => ({ Lemon: 'Lemon' })
-	//     );
-	// console.log(fruits);
-
-	immutablePush(array, element) {
-		return [ ...array, element ];
-	}
-
-	update(cellX, cellY) {
-		let indexX = cellX / gridSize;
-		let indexY = cellY / gridSize;
-		this.grid[indexY][indexX].taken = true;
-		console.log(indexX, indexY, this.grid);
-	}
-
-	show() {
-		this.grid.map((row, rowIndex) => {
-			row.map((singleCell, colIndex) => {
-				singleCell.taken ? fill(255) : noFill();
-				rect(colIndex * gridSize, rowIndex * gridSize, gridSize, gridSize);
-			});
-		});
-	}
+    show() {
+        this.grid.map((row, rowIndex) => {
+            row.map((singleCell, colIndex) => {
+                singleCell.taken ? fill(255) : noFill();
+                rect(
+                    colIndex * gridSize,
+                    rowIndex * gridSize,
+                    gridSize,
+                    gridSize
+                );
+            });
+        });
+    }
 }
